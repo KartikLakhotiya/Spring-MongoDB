9307ab3a09b576e1bb9ba0c0a1a4f1f66e6b016f
Kartik  Lakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-11-17 23:52:02 +0530

    search function added

--- src/main/java/com/example/joblisting/controller/PostController.java
+++ src/main/java/com/example/joblisting/controller/PostController.java
@@ -1,13 +1,18 @@
 package com.example.joblisting.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import com.example.joblisting.repository.PostRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.example.joblisting.model.Post;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.joblisting.model.Post;
+import com.example.joblisting.repository.PostRepository;
+import com.example.joblisting.repository.SearchRepository;
+
 
 @RestController
 public class PostController {
@@ -15,9 +20,17 @@ public class PostController {
     @Autowired
     PostRepository repo;
 
+    @Autowired
+    SearchRepository searchrepo;
+
     @GetMapping("/allposts")
     public List<Post> getAllPosts() {
         return repo.findAll();
+    }
+
+    @GetMapping("/posts/{text}")
+    public List<Post> search(@PathVariable String text) {
+        return searchrepo.findByText(text);
     }
 
     @PostMapping("/addpost")
