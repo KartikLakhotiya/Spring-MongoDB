8a8a788894e5ecdf2f34d669d9bb3ef0ffea89d2
Kartik  Lakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-11-17 21:49:53 +0530

    add post endpoint completed

--- src/main/java/com/example/joblisting/controller/PostController.java
+++ src/main/java/com/example/joblisting/controller/PostController.java
@@ -1,16 +1,13 @@
 package com.example.joblisting.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.joblisting.repository.PostRepository;
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.example.joblisting.model.Post;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class PostController {
@@ -18,9 +15,14 @@ public class PostController {
     @Autowired
     PostRepository repo;
 
-    @GetMapping("/posts")
+    @GetMapping("/allposts")
     public List<Post> getAllPosts() {
         return repo.findAll();
+    }
+
+    @PostMapping("/addpost")
+    public Post addPost(@RequestBody Post post){
+        return repo.save(post);
     }
     
 }
