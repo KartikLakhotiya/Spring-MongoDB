f4028baa7d40d531b367f0deb3346792bd96484b
Kartik  Lakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-11-17 23:55:58 +0530

    cors configuration added

--- src/main/java/com/example/joblisting/controller/PostController.java
+++ src/main/java/com/example/joblisting/controller/PostController.java
@@ -3,6 +3,7 @@ package com.example.joblisting.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +14,10 @@ import com.example.joblisting.model.Post;
 import com.example.joblisting.repository.PostRepository;
 import com.example.joblisting.repository.SearchRepository;
 
-
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class PostController {
-    
+
     @Autowired
     PostRepository repo;
 
@@ -24,18 +25,23 @@ public class PostController {
     SearchRepository searchrepo;
 
     @GetMapping("/allposts")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Post> getAllPosts() {
         return repo.findAll();
     }
 
     @GetMapping("/posts/{text}")
+    @CrossOrigin(origins = "http://localhost:3000")
+
     public List<Post> search(@PathVariable String text) {
         return searchrepo.findByText(text);
     }
 
     @PostMapping("/addpost")
-    public Post addPost(@RequestBody Post post){
+    @CrossOrigin(origins = "http://localhost:3000")
+
+    public Post addPost(@RequestBody Post post) {
         return repo.save(post);
     }
-    
+
 }
